a5b84f89ba019a8daf7db6e36610fdb0b1bf3c5a
TannerL <tannerlaw@weber.edu>
2022-02-06 19:47:46 -0700

    first commit

---
+++ Readme.md
@@ -0,0 +1,1 @@
+# SmoothStackD1A1
---
+++ history.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# *** Spyder Python Console History Log ***
+
+## ---(Sat Feb  5 08:53:48 2022)---
+runfile('C:/Users/owner/.spyder-py3/temp.py', wdir='C:/Users/owner/.spyder-py3')
+runcell(0, 'C:/Users/owner/.spyder-py3/temp.py')
+runfile('C:/Users/owner/.spyder-py3/temp.py', wdir='C:/Users/owner/.spyder-py3')
+runcell(0, 'C:/Users/owner/.spyder-py3/temp.py')
+debugfile('C:/Users/owner/.spyder-py3/temp.py', wdir='C:/Users/owner/.spyder-py3')
+runfile('C:/Users/owner/.spyder-py3/temp.py', wdir='C:/Users/owner/.spyder-py3')
+runcell(0, 'C:/Users/owner/.spyder-py3/temp.py')
+debugfile('C:/Users/owner/.spyder-py3/temp.py', wdir='C:/Users/owner/.spyder-py3')
+runfile('C:/Users/owner/.spyder-py3/temp.py', wdir='C:/Users/owner/.spyder-py3')
